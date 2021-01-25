67e807a4768196bcec3b33e77c937308974eff7f
Quentin Burgess <qutn.burgess@gmail.com>
2021-01-25 11:08:55 +0100

    moki:api: update node setup regarding :5000 listing

--- Moki/server/src/app.js
+++ Moki/server/src/app.js
@@ -7,8 +7,10 @@ const { cfg } = require('./modules/config');
 
 // const port = process.env.PORT || 5000;
 // start server
-const server = app.listen(cfg.port, () => {
-  // eslint-disable-next-line no-console
+let server = app.listen(cfg.port, '127.0.0.1');
+
+// eslint-disable-next-line no-console
+server.on('listening', () => {
   console.log(`listening on:\t${cfg.port}`);
 }).on('error', console.log);
 
