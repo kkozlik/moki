826ca7be5978e427df3c0d67e353c333041c4c78
Jana Němcová <ja_nemcova@volny.cz>
2021-06-08 17:12:21 +0200

    F5629: adding new category

--- Moki/client/src/js/pages/general_settings.jsx
+++ Moki/client/src/js/pages/general_settings.jsx
@@ -141,7 +141,6 @@ class Settings extends Component {
             var result = [];
             for (var i = 0; i < jsonData.length; i++) {
                 var data = document.getElementById(jsonData[i].attribute);
-
                 if (data.type === "checkbox") {
                     jsonData[i].value = data.checked;
 
@@ -162,7 +161,6 @@ class Settings extends Component {
                     attribute: jsonData[i].attribute,
                     value: jsonData[i].value
                 });
-
 
             };
             this.setState({
@@ -207,66 +205,66 @@ class Settings extends Component {
         if (data.length !== 0) {
             // Outer loop to create parent
             for (var i = 0; i < data.length; i++) {
-                alarms.push( <div key = {
-                        data[i].attribute + "key"
-                    }
-                    className = "tab "> <span className = "form-inline row justify-content-start paddingBottom"> <label className = "col-7" > {
+                alarms.push(<div key={
+                    data[i].attribute + "key"
+                }
+                    className="tab "> <span className="form-inline row justify-content-start paddingBottom"> <label className="col-7" > {
                         data[i].label
                     } </label>
-                     {data[i].type === "boolean" ? 
-                     data[i].value ? <input className="text-left form-check-input" type="checkbox" defaultChecked="true"  id={data[i].attribute} /> :
-                    <input className="text-left form-check-input" type="checkbox"   id={data[i].attribute} />
-                    :  <input className="text-left form-control form-check-input" type={data[i].type } defaultValue={data[i].value} id={data[i].attribute} />
-                    }
-                    </span></div> );
+                        {data[i].type === "boolean" ?
+                            data[i].value ? <input className="text-left form-check-input" type="checkbox" defaultChecked="true" id={data[i].attribute} /> :
+                                <input className="text-left form-check-input" type="checkbox" id={data[i].attribute} />
+                            : <input className="text-left form-control form-check-input" type={data[i].type} defaultValue={data[i].value} id={data[i].attribute} />
+                        }
+                    </span></div>);
             }
         }
         return alarms
     }
 
     generateTags() {
-            var data = this.state.tags;
+        var data = this.state.tags;
 
-            var tags = [];
-            if (data.length !== 0) {
-                for (var i = 0; i < data.length; i++) {
-                    tags.push( <div key = {
-                            data[i].key
-                        }
-                        className = "tab" > <span className = "form-inline row justify-content-start paddingBottom" >
-                        <img className = "icon"
-                        alt = "deleteIcon"
-                        src = {
-                            deleteIcon
-                        }
-                        title = "delete tag"
-                        onClick = {
-                            this.deleteTag.bind(this)
-                        }
-                        id = {
-                            data[i].key
-                        }
-                        /> <label className = "col-4" > {
+        var tags = [];
+        if (data.length !== 0) {
+            for (var i = 0; i < data.length; i++) {
+                tags.push(<div key={
+                    data[i].key
+                }
+                    className="tab" > <span className="form-inline row justify-content-start paddingBottom" >
+                        <img className="icon"
+                            alt="deleteIcon"
+                            src={
+                                deleteIcon
+                            }
+                            title="delete tag"
+                            onClick={
+                                this.deleteTag.bind(this)
+                            }
+                            id={
+                                data[i].key
+                            }
+                        /> <label className="col-4" > {
                             data[i].key + " (" + data[i].doc_count + ")"
-                        } </label> </span> </div> );      
-                    }
-                } else {
-                    tags.push( < div className = "tab" key="notags"> No tags. Create them directly in event's table.</div> );  
+                        } </label> </span> </div>);
+            }
+        } else {
+            tags.push(< div className="tab" key="notags"> No tags. Create them directly in event's table.</div>);
 
-                    }
-                    return tags
+        }
+        return tags
     }
 
     async deleteTag(e) {
         var tag = e.currentTarget.id;
-        var data = await elasticsearchConnection("/api/tag/delete", {id: "", index: "", tags: tag});
+        var data = await elasticsearchConnection("/api/tag/delete", { id: "", index: "", tags: tag });
         if (data.deleted >= 0) {
-               var tags = this.state.tags;         
-               for (var i = 0; i < tags.length; i++) {
+            var tags = this.state.tags;
+            for (var i = 0; i < tags.length; i++) {
                 if (tags[i].key === tag) {
                     tags.splice(i, 1);
                 }
-               }
+            }
             //this.props.getTags();
             this.setState({
                 tags: tags
@@ -278,79 +276,86 @@ class Settings extends Component {
     }
 
 
-        render() {
+    render() {
 
-            var data = this.state.data;
-            var General = [];
-            var Slowlog = [];
-            var LE = [];
-            var Firewall = [];
-            var Events = [];
+        var data = this.state.data;
+        var General = [];
+        var Slowlog = [];
+        var LE = [];
+        var Firewall = [];
+        var Events = [];
+        var Auth = [];
 
-            //separate type
-            for (var i = 0; i < data.length; i++) {
-                if (data[i].category === "General") {
-                    General.push(data[i]);
-                } else if (data[i].category === "Slowlog") {
-                    Slowlog.push(data[i]);
-                } else if (data[i].category === "Logstash and elasticsearch") {
-                    LE.push(data[i]);
-                } else if (data[i].category === "Firewall") {
-                    Firewall.push(data[i]);
-                } else if (data[i].category === "Events") {
-                    Events.push(data[i]);
-                }
+        //separate type
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].category === "General") {
+                General.push(data[i]);
+            } else if (data[i].category === "Slowlog") {
+                Slowlog.push(data[i]);
+            } else if (data[i].category === "Logstash and elasticsearch") {
+                LE.push(data[i]);
+            } else if (data[i].category === "Firewall") {
+                Firewall.push(data[i]);
+            } else if (data[i].category === "Events") {
+                Events.push(data[i]);
             }
-
-            var Generaldata = this.generate(General);
-            var Slowlogdata = this.generate(Slowlog);
-            var LEdata = this.generate(LE);
-            var Firewalldata = this.generate(Firewall);
-            var Eventsdata = this.generate(Events);
-            var Tagdata = this.generateTags();
-
-
-            return ( <div className = "container-fluid" > {
-                    this.state.wait && < SavingScreen />
-                } <p className = "settingsH" > General </p> {
-                    Generaldata
-                }
-
-                <p className = "settingsH" > Firewall </p> {
-                    Firewalldata
-                }
-
-                <p className = "settingsH" > Events </p> {
-                    Eventsdata
-                } <p className = "settingsH" > Elasticsearch and logstash </p> {
-                    LEdata
-                } <p className = "settingsH" > Slowlog </p> {
-                    Slowlogdata
-                } <p className = "settingsH" > Tags </p> {
-                    Tagdata
-                }
-
-
-                <div className = "btn-group rightButton" >
-                <button type = "button"
-                className = "btn btn-primary "
-                onClick = {
-                    this.save
-                }
-                style = {
-                    {
-                        "marginRight": "5px"
-                    }
-                } > Save </button> <button type = "button"
-                className = "btn btn-secondary"
-                onClick = {
-                    () => this.load("api/defaults")
-                } > Reset </button> </div >
-
-                </div>
-            )
+            else if (data[i].category === "Authentication") {
+                Auth.push(data[i]);
+            }
         }
 
+        var Generaldata = this.generate(General);
+        var Slowlogdata = this.generate(Slowlog);
+        var LEdata = this.generate(LE);
+        var Firewalldata = this.generate(Firewall);
+        var Eventsdata = this.generate(Events);
+        var Authdata = this.generate(Auth);
+        var Tagdata = this.generateTags();
+
+
+        return (<div className="container-fluid" > {
+            this.state.wait && < SavingScreen />
+        } <p className="settingsH" > General </p> {
+                Generaldata
+            }
+
+            <p className="settingsH" > Firewall </p> {
+                Firewalldata
+            }
+            <p className="settingsH" > Authentication </p> {
+                Authdata
+            }
+            <p className="settingsH" > Events </p> {
+                Eventsdata
+            } <p className="settingsH" > Elasticsearch and logstash </p> {
+                LEdata
+            } <p className="settingsH" > Slowlog </p> {
+                Slowlogdata
+            } <p className="settingsH" > Tags </p> {
+                Tagdata
+            }
+
+
+            <div className="btn-group rightButton" >
+                <button type="button"
+                    className="btn btn-primary "
+                    onClick={
+                        this.save
+                    }
+                    style={
+                        {
+                            "marginRight": "5px"
+                        }
+                    } > Save </button> <button type="button"
+                        className="btn btn-secondary"
+                        onClick={
+                            () => this.load("api/defaults")
+                        } > Reset </button> </div >
+
+        </div>
+        )
     }
 
-    export default Settings;
+}
+
+export default Settings;
