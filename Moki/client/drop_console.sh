8fd32ba0dc49e678dfb87019e8163bf1ba89b479
Cristian Constantin <cristian@intuitivelabs.com>
2021-07-29 14:38:17 +0200

    terser should not use source map if its not there

--- Moki/client/drop_console.sh
+++ Moki/client/drop_console.sh
@@ -7,7 +7,13 @@ fi
 DIR=`mktemp -d`
 
 for i in build/static/js/*.js; do
-    npx terser $i --source-map "content='${i}.map'" -c drop_console=true -o ${DIR}/`basename $i` ;
+    if [[ -v GENERATE_SOURCEMAP ]] && [[ $GENERATE_SOURCEMAP == 'false' ]]; then
+        # w/o source map
+        npx terser $i -c drop_console=true -o ${DIR}/`basename $i` ;
+    else
+        # with source map
+        npx terser $i --source-map "content='${i}.map'" -c drop_console=true -o ${DIR}/`basename $i` ;
+    fi
 done
 
 if [[ ! -d build/static/js_orig ]]; then
