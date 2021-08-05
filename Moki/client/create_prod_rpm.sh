bc7fc3e2832ba2e6a2e4ea4e9763d7ad007d7ac3
Cristian Constantin <cristian@intuitivelabs.com>
2021-08-05 21:04:16 +0200

    groups env settings; uses Makefile's clean/rpm

--- Moki/client/create_prod_rpm.sh
+++ Moki/client/create_prod_rpm.sh
@@ -5,13 +5,22 @@
 
 #DIR=Moki/client
 REP=$branch_name
+REPOTYPE="prod"
 NAME=moki-client
 SPEC=$NAME.spec
 # architecture - static for now
 RPMARCH=x86_64
 RPM_SRC_DIR="/var/lib/jenkins/rpmbuild/RPMS/x86_64"
 # repository type - dev for this automatic build
-REPOTYPE="prod"
+RPM_VERSION=`cat $SPEC|grep -e "^Version:"|awk '{print $2}'`
+# repo location
+if [[ "$branch" == "master" ]] ; then 
+	RPM_REPO_DIR="$HOME/repointernal/rpm/dev/$RPMARCH"
+else
+	RPM_REPO_DIR="$HOME/repointernal/rpm/branch/$branch/$RPMARCH"
+fi
+
+sed -i "s/Release:.*/Release:\t$BUILD_NUMBER/" $SPEC
 
 # vendorize the package.json
 # use il package.json
@@ -23,30 +32,14 @@ ln -sf package.json package-intuitive.json
 rm -rf node_modules
 #sed -i 's/"homepage.*/"homepage": ".",/g' package.json
 
-
-RPM_VERSION=`cat $SPEC|grep -e "^Version:"|awk '{print $2}'`
-sed -i "s/Release:.*/Release:\t$BUILD_NUMBER/" $SPEC
-
 # clean package-lock.json
 #rm -f package-lock.json
 
 # build the package
-make rpmtar
-mkdir -p ~/rpmbuild/SOURCES
-cp $NAME-${RPM_VERSION}-${BUILD_NUMBER}.tar.gz ~/rpmbuild/SOURCES
-
-cat $NAME.spec
-
-rpmbuild --clean -bb $NAME.spec
+make clean
+make rpm
 
 ### upload rpms
-
-# repo location
-if [[ "$branch" == "master" ]] ; then 
-	RPM_REPO_DIR="$HOME/repointernal/rpm/dev/$RPMARCH"
-else
-	RPM_REPO_DIR="$HOME/repointernal/rpm/branch/$branch/$RPMARCH"
-fi
 
 mkdir -p $RPM_REPO_DIR
 
