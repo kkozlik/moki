a30cd5ad95191c659e37d654fc10b08398b7bdc2
Vladimir Broz <vlada@intuitivelabs.com>
2021-08-30 21:34:18 +0200

    parameter for rpm type ("private") or public by default

--- Moki/client/create_rpm.sh
+++ Moki/client/create_rpm.sh
@@ -10,6 +10,16 @@
 #			    cuted.  Useful for debugging.
 set -ex
 
+# check type of build - public / private Moki
+if [[ "$1" == "private" ]] ; then
+	REPO_TYPE="repointernal"
+	# vendorize the package.json
+	# use il package.json
+	ln -sf package-intuitive.json package.json
+else
+	REPO_TYPE="repopublic"
+fi
+
 ## configuration and variables section
 REP=$branch_name
 # name of the production build
@@ -25,11 +35,11 @@ RPM_SRC_DIR="/var/lib/jenkins/rpmbuild/RPMS/x86_64"
 RPM_VERSION=`cat $SPEC|grep -e "^Version:"|awk '{print $2}'`
 # repo location
 if [[ "$branch" == "master" ]] ; then 
-	RPM_LOCAL_REPO_DIR="$HOME/repointernal/rpm/dev/$RPMARCH"
-	RPM_REMOTE_REPO_URL="s3://repointernal/rpm/dev/$RPMARCH"
+	RPM_LOCAL_REPO_DIR="$HOME/$REPO_TYPE/rpm/dev/$RPMARCH"
+	RPM_REMOTE_REPO_URL="s3://$REPO_TYPE/rpm/dev/$RPMARCH"
 else
-	RPM_LOCAL_REPO_DIR="$HOME/repointernal/rpm/branch/$branch/$RPMARCH"
-	RPM_REMOTE_REPO_URL="s3://repointernal/rpm/branch/$branch/$RPMARCH"
+	RPM_LOCAL_REPO_DIR="$HOME/$REPO_TYPE/rpm/branch/$branch/$RPMARCH"
+	RPM_REMOTE_REPO_URL="s3://$REPO_TYPE/rpm/branch/$branch/$RPMARCH"
 fi
 
 ## code section
@@ -37,11 +47,6 @@ fi
 sed -i "s/Release:.*/Release:\t$BUILD_NUMBER/" $SPEC
 sed -i "s/Release:.*/Release:\t$BUILD_NUMBER/" $SPEC_DEV
 
-# vendorize the package.json
-# use il package.json
-ln -sf package-intuitive.json package.json
-
-#cd $DIR
 
 #FIXES
 rm -rf node_modules
@@ -63,7 +68,6 @@ make rpm
 ### upload rpms
 
 mkdir -p $RPM_LOCAL_REPO_DIR
-
 if test -f $RPM_SRC_DIR/${NAME_DEV}-${RPM_VERSION}-${BUILD_NUMBER}.x86_64.rpm ; then
   mv $RPM_SRC_DIR/${NAME_DEV}-${RPM_VERSION}-${BUILD_NUMBER}.x86_64.rpm $RPM_LOCAL_REPO_DIR
 fi
