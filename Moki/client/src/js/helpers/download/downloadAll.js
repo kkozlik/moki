b747d46a8e51be1da84bac3d8cca509cd8da7ce2
Jana Němcová <ja_nemcova@volny.cz>
2021-09-06 16:23:08 +0200

    export fix

--- Moki/client/src/js/helpers/download/downloadAll.js
+++ Moki/client/src/js/helpers/download/downloadAll.js
@@ -18,33 +18,38 @@ export async function downloadAll(obj) {
 
     //export json (always exists)
     var json = new Blob([JSON.stringify(obj)], { type: 'text/plain' });
-    if(storePersistent.getState().profile[0].userprefs.mode === "encrypt"){
+    if (storePersistent.getState().profile[0].userprefs.mode === "encrypt") {
         fileName = fileName + "_decrypted";
     }
     zip.file(fileName + ".json", json);
 
-    //download sd
-    var sd = await downloadSD(obj.attrs.filename);
-    if (sd && !sd.includes("Error")) {
-        zip.file(fileName + ".html", sd);
+    if (obj.attrs.filename) {
+        //download sd
+        var sd = await downloadSD(obj.attrs.filename);
+        if (sd && !sd.includes("Error")) {
+            zip.file(fileName + ".html", sd);
+        }
     }
 
-
-    //download pcap
-    await downloadPcap(obj.attrs.filename).then(function (data) {
-        fileName = fileName ? fileName.substring(fileName.lastIndexOf("/") + 1) : Math.random().toString(36).substring(7);
-        if (typeof data !== 'string' || !data.includes("ERROR")) {
-            var blob = new Blob([data], { type: "pcap" });
-            zip.file(fileName + ".pcap", blob);
-        }
-    })
+    if (obj.attrs.filename) {
+        //download pcap
+        await downloadPcap(obj.attrs.filename).then(function (data) {
+            fileName = fileName ? fileName.substring(fileName.lastIndexOf("/") + 1) : Math.random().toString(36).substring(7);
+            if (typeof data !== 'string' || !data.includes("ERROR")) {
+                var blob = new Blob([data], { type: "pcap" });
+                zip.file(fileName + ".pcap", blob);
+            }
+        })
+    }
 
     zip.generateAsync({ type: "blob" })
         .then(function (blob) {
             var name = "export.zip";
-            if(storePersistent.getState().profile[0].userprefs.mode === "encrypt"){
-               name = "export_decrypted.zip";
+            if (storePersistent.getState().profile[0].userprefs.mode === "encrypt") {
+                name = "export_decrypted.zip";
             }
+            console.log(name);
+
             FileSaver.saveAs(blob, name);
         });
 }
