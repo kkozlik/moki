f513e3d7cc21156de6d3ea340bbeac250c5d23c2
Jana Němcová <ja_nemcova@volny.cz>
2021-10-26 09:04:30 +0200

    change loading screen

--- Moki/client/src/js/helpers/LoadingScreenCharts.jsx
+++ Moki/client/src/js/helpers/LoadingScreenCharts.jsx
@@ -2,15 +2,17 @@ import React, {
     Component
 } from 'react';
 
-class LoadingScreenCharts extends Component{   
-    render(){
-        return(
-               <div className="row align-items-center justify-content-center overlay" style={{left: 0}}>
-                    <div className="loaderSmall"/>
-                    <h2 style={{"marginLeft": "10px"}} >Getting data...</h2>
-                </div>        
+class LoadingScreenCharts extends Component {
+    render() {
+        return (
+            <div className="row align-items-center justify-content-center overlay" style={{ left: 0 }}>
+                <div class="loaderr">
+                    <div class="bar"></div>
+                </div>
+                <div style={{"position": "absolute", "marginBottom": "30px"}}><h2 style={{ "marginLeft": "10px", "fontSize": "medium", "color": " var(--main)" }} >LOADING DATA</h2></div>
+            </div>
         )
-        }
     }
-    
+}
+
 export default LoadingScreenCharts;
