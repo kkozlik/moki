7481beb3501963604ce158dee44e2d6e69621cb3
Jana Němcová <ja_nemcova@volny.cz>
2021-12-27 11:29:17 +0100

    fix export for admin

--- Moki/client/src/js/helpers/export.js
+++ Moki/client/src/js/helpers/export.js
@@ -6,7 +6,7 @@ import storePersistent from "../store/indexPersistent";
 export async function exportJSON(result) {
     const element = document.createElement("a");
     var file = "";
-    if(storePersistent.getState().profile[0].userprefs.mode === "encrypt"){
+    if( storePersistent.getState().profile && storePersistent.getState().profile[0] && storePersistent.getState().profile[0].userprefs.mode === "encrypt"){
         element.download = "data_decryted.json"
     }else {
         element.download = "data.json";
