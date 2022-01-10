8092510ce83e08c5cba5cf7cc55e497d14f5d518
Jana Němcová <ja_nemcova@volny.cz>
2022-01-10 20:57:21 +0100

    fix timestamp format

--- Moki/client/src/js/helpers/TableColumns.jsx
+++ Moki/client/src/js/helpers/TableColumns.jsx
@@ -335,7 +335,7 @@ function getColumn(column_name, tags, tag) {
                     headerStyle: { width: '170px' },
                     formatter: (cell, obj) => {
                         var ob = obj._source[column_name.source];
-                        if(parseTimestamp(ob) instanceof Date && isFinite(parseTimestamp(ob))){
+                        if(parseTimestamp(ob) !== "Invalid date" ){
                             return parseTimestamp(ob)
                         }
                         else {
