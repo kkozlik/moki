8b0099c40d802e5b3489268889cee53c67d3fe02
Jana Němcová <ja_nemcova@volny.cz>
2022-01-11 15:00:12 +0100

    make list chart generic

--- Moki/client/src/js/controllers/paths.jsx
+++ Moki/client/src/js/controllers/paths.jsx
@@ -14,7 +14,11 @@ function newComponent(name, tags, hostnames, dstRealms, srcRealms, showError) {
 //returns path list from dashboard array
 export const paths = (dashboards, tags, hostnames, dstRealms, srcRealms, showError) => {
     var paths = [];
-    paths = dashboards.map(e => <Route key={e} exact path={'/' + e} render={() => newComponent(e, tags, hostnames, dstRealms, srcRealms, showError)} />) 
+    paths = dashboards.map(e => <Route key={e} exact path={'/' + e} render={() => newComponent(e, tags, hostnames, dstRealms, srcRealms, showError)} />);
+    console.log(componentList.tenant);
+    if (componentList.tenant) {
+        paths.push(<Route key="tenant" exact path={"/tenant"} render={() => newComponent("tenant", tags, hostnames, dstRealms, srcRealms, showError)} />);
+    }
     return paths;
 
 }
