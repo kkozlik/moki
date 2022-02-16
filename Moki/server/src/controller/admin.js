124b09e4c1bfce3b5782a2e9830284d1cef656d2
Jana Němcová <ja_nemcova@volny.cz>
2022-02-16 08:28:22 +0100

    password with spaces

--- Moki/server/src/controller/admin.js
+++ Moki/server/src/controller/admin.js
@@ -247,7 +247,7 @@ class AdminController {
 create new user with password in htpasswd
 */
   static async createUser(req, res) {
-    exec("sudo htpasswd -b -c " + cfg.htpasswd + " " + req.body.name + " " + req.body.password, (error, stdout, stderr) => {
+    exec("sudo htpasswd -b -c "+cfg.htpasswd+ " '" + req.body.name + "' '" + req.body.password+"'", (error, stdout, stderr) => {
       if (error) {
         console.error(`Can't create new user in nginx : ${error.message}`);
         return res.json({ "error": error.message });
