e2dabedfc69a3c36f44b585dda10956b624b1eac
Jana Němcová <ja_nemcova@volny.cz>
2022-03-14 21:59:50 +0100

    Update countryFlag.jsx

--- Moki/client/src/js/helpers/countryFlag.jsx
+++ Moki/client/src/js/helpers/countryFlag.jsx
@@ -4,21 +4,34 @@ class CountryFlag extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            picture: null
+            picture: null,
+            countryCode: null
         }
         this.getCountryFlag = this.getCountryFlag.bind(this);
     }
 
     componentDidMount() {
+        this.setState({ countryCode: this.props.countryCode });
         this.getCountryFlag(this.props.countryCode);
+    }
+
+
+      //after redirect delete unpinned filters
+      componentWillReceiveProps(nextProps) {
+        if (nextProps.countryCode !== this.props.countryCode) {
+            this.setState({ countryCode: nextProps.countryCode });
+            this.getCountryFlag(nextProps.countryCode);
+        }
     }
 
     async getCountryFlag(countryCode) {
         try {
             const picture = (await import(`../../styles/flags/${countryCode.toLowerCase()}.png`));
             if(picture.default)   this.setState({ picture: picture.default });
+            else this.setState({ picture: null });
         }
         catch (err) {
+            this.setState({ picture: null });
             //Do whatever you want when the image failed to load here
         }
     }
