cc2fe8437e88dc233e22c565e31e5d43f9e8bce5
Jana Němcová <ja_nemcova@volny.cz>
2022-03-24 12:04:23 +0100

    use es module

--- Moki/server/src/controller/admin.js
+++ Moki/server/src/controller/admin.js
@@ -1,4 +1,4 @@
-const elasticsearch = require('elasticsearch');
+const { connectToES } = require('../modules/elastic');
 const { parseBase64 } = require('../modules/jwt');
 const { isRequireJWT } = require('../modules/config');
 const { exec } = require("child_process");
@@ -50,7 +50,7 @@ class AdminController {
      * events: {timestamp, userID, domain, level} type=login
      */
     async function storeLoginInES(domain, userID, jwtbit, email, sourceIP) {
-      const client = new elasticsearch.Client({ host: process.env.ES, requestTimeout: 60000 });
+      const client = connectToES();
       const now = new Date();
       const index = "lastlog-" + now.getFullYear() + "." + (now.getMonth() + 1);
       const existIndex = await client.indices.exists({ index: index });
