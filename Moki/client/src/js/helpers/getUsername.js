eaad5343ae9b03ce3ac7c576a0a095b6aaabfc05
Jana Němcová <ja_nemcova@volny.cz>
2022-08-31 15:45:30 +0200

    rerender logged user

---
+++ Moki/client/src/js/helpers/getUsername.js
@@ -0,0 +1,28 @@
+//get username from server, store in redux and also return it
+
+import storePersistent from "../store/indexPersistent";
+import { setUser } from "./js/actions/index";
+
+export async function getUsername() {
+    var url = process.env.PUBLIC_URL + "/api/user/username";
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": "include"
+            }
+        });
+        let username = await response.json();
+        if (storePersistent.getState().user) {
+            let user = storePersistent.getState().user;
+            user.username = username.username;
+            storePersistent.dispatch(setUser(user));
+        }
+        return username.username;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
