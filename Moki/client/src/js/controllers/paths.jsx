59486af5b56f2fd1082193055f0bea12c8cd65d2
Jana Němcová <ja_nemcova@volny.cz>
2022-09-05 08:40:05 +0200

    remove old code

--- Moki/client/src/js/controllers/paths.jsx
+++ Moki/client/src/js/controllers/paths.jsx
@@ -6,14 +6,14 @@ import {
     Route
 } from 'react-router-dom';
 
-function newComponent(name, tags, hostnames, dstRealms, srcRealms, showError) {
+function newComponent(name, tags, hostnames, dstRealms, srcRealms) {
     var ComponentToRender = componentList[name];
-    return <ComponentToRender tags={tags} name={name} showError={showError} hostnames={hostnames} dstRealms={dstRealms} srcRealms={srcRealms} />;
+    return <ComponentToRender tags={tags} name={name}  hostnames={hostnames} dstRealms={dstRealms} srcRealms={srcRealms} />;
 }
 
 //returns path list from dashboard array
-export const paths = (dashboards, tags, hostnames, dstRealms, srcRealms, showError) => {
+export const paths = (dashboards, tags, hostnames, dstRealms, srcRealms) => {
     var paths = [];
-    paths = dashboards.map(e => <Route key={e} exact path={'/' + e} render={() => newComponent(e, tags, hostnames, dstRealms, srcRealms, showError)} />);
+    paths = dashboards.map(e => <Route key={e} exact path={'/' + e} render={() => newComponent(e, tags, hostnames, dstRealms, srcRealms)} />);
     return paths;
 }
