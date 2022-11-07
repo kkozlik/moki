2db6d9e82ca348ebb425243944eb32b91a4ba04a
Jana Nemcova <jana@intuitivelabs.com>
2022-11-07 12:51:43 +0100

    Graph packet drop alert events' counters in network

--- Moki/client/src/js/dashboards/Network/NetworkCharts.js
+++ Moki/client/src/js/dashboards/Network/NetworkCharts.js
@@ -32,7 +32,8 @@ class NetworkCharts extends Dashboard {
             greylist: [],
             whitelist: [],
             isLoading: true,
-            hostnames: []
+            hostnames: [],
+            dropAlert: []
 
         };
         this.callBacks = {
@@ -80,7 +81,10 @@ class NetworkCharts extends Dashboard {
               [{result: 'greylist', func: parseMultipleLineData}],
 
               //IPS ON FW WHITELIST BY HOST
-              [{result: 'whitelist', func: parseMultipleLineData}]
+              [{result: 'whitelist', func: parseMultipleLineData}],
+
+               //packet drop alert events' counters
+               [{result: 'dropAlert', func: parseMultipleLineData}]
             ]
         };
     }
@@ -176,6 +180,10 @@ class NetworkCharts extends Dashboard {
                         />
                     </div>
 
+                    <div className="col-6 px-1">
+                        <MultipleLineChart id="dropAlert" hostnames={this.state.hostnames} data={this.state.dropAlert} name={"PACKET DROP ALERT COUNTERS"} ticks={3}
+                        />
+                    </div>
                 </div>
             </div>
         );
