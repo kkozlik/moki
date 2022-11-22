17f8569b4a17a98493e618fc7ea612df71362b1d
Jana Nemcova <jana@intuitivelabs.com>
2022-11-22 12:30:56 +0100

    import font-awesome

--- Moki/client/src/index.js
+++ Moki/client/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/src/jquery';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
